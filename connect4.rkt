9fae78fd85743f3f51b7dbb2f4e34e346cee47ec
JusungLee0601 <jusunglee0601@gmail.com>
2020-05-02 06:11:32 -0400

    Dimensions and Trace Setup
    Added 4 by 4 with a 3 connect win. Need to finish traces allowing for gravity with moves.

--- connect4.rkt
+++ connect4.rkt
@@ -10,19 +10,27 @@ one sig B extends Index {}
 one sig C extends Index {}
 one sig D extends Index {}
 
-sig Board {places: set Index -> Index -> Player}
+sig Board {places: set Index -> Index -> Player,
+           first3: set Index,
+           second3: set Index
+}
 
-pred wellformed {
+pred wellformed4by4 {
   //constraints on sets
   Player = P1 + P2
   Index = A + B + C + D
   inverse = A->D + B->C + C->B + D->A
+  first3 = A + B + C
+  second3 = B + C + D
 
   //only one board position with its indices
   all b: Board | all r: Index | all c: Index | {
     lone r.(c.(b.places))    
   }
 }
+
+
+----------------------turns-----------------------
 
 pred xturn[brd: Board] {
     #brd.places.X = #brd.places.O  
@@ -32,20 +40,45 @@ pred oturn[b: Board] {
     subtract[#b.places.X,1] = #b.places.O
 }
 
+
+
+------------------win conditions------------------
+
+//horizontal
+pred winHf[b: Board, p: Player] {
+  some r: Index | all c: b.first3 |
+    p in b.places[r][c]
+}
+pred winHs[b: Board, p: Player] {
+  some r: Index | all c: b.second3 |
+    p in b.places[r][c]
+}
 pred winH[b: Board, p: Player] {
-  some r: Index | all c: Index |
-    p in b.places[r][c]
+  winHf[b, p] or winHs[b, p]
 }
 
+//vertical
+pred winVf[b: Board, p: Player] {
+  some c: Index | all r: b.first3 |
+    p in b.places[r][c]
+}
+pred winVs[b: Board, p: Player] {
+  some c: Index | all r: b.second3 |
+    p in b.places[r][c]
+}
 pred winV[b: Board, p: Player] {
-  some c: Index | all r: Index |
-    p in b.places[r][c]
+  winVf[b, p] or winVs[b, p]
 }
 
+//diagonal
 pred winD[b: Board, p: Player] {
-  (all i: Index | i->i->p in b.places)
+  (all i: b.first3 | i->i->p in b.places)
   or
-  (all i: Index | i->i.inverse->p in b.places)
+  (all i: b.second3 | i->i->p in b.places)
+  or
+  (all i: b.first3 | i->i.inverse->p in b.places)
+  or
+  (all i: b.second3 | i->i.inverse->p in b.places)
 }
 
 pred winning[b: Board, p: Player] {
