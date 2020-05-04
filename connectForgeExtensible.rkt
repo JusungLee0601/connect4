2b5b4ea760f011b744d196b9d46e05743fc4680a
JusungLee0601 <jusunglee0601@gmail.com>
2020-05-03 22:03:47 -0400

    Renaming and Final Touches

--- connectForgeExtensible.rkt
+++ connectForgeExtensible.rkt
@@ -38,7 +38,7 @@ pred wellformed {
     }
 
     //Two players cannot have played the same index
-    no Board.p1 & Board.p2 //test if necessary?
+    no Board.p1 & Board.p2
 }
 
 ----------------Turns----------------
@@ -48,7 +48,7 @@ pred p1_turn[b: Board] {
     #(b.p1) = #(b.p2)
 }
 
-//p2's turn otherwise
+//p2's turn, p1 is always within one move
 pred p2_turn[b: Board] {
     subtract[#(b.p1),1] = #(b.p2)
 }
@@ -109,8 +109,31 @@ pred won[p: set Row->Column] {
     vertical_w[p] or diagonal_w[p] or horizontal_w[p]
 }
 
+//Either p1 or p2 wins
 pred finished[b: Board] {
     won[b.p1] or won[b.p2]
+}
+
+//Check if p1 can win
+pred p1_canwin[b: Board] {
+    //Look for index at (r,c) such that:
+    some r: Row | some c: Column | {
+        //Available space
+        not r->c in b.p1 + b.p2
+        //Bottom row or has a piece below it
+        some r.prev_row implies r.prev_row->c in b.p1 + b.p2
+        //Allows p1 to win the game
+        won[b.p1 + r->c]
+    }
+}
+
+//Check if p2 can win
+pred p2_canwin[b: Board] {
+    some r: Row | some c: Column | {
+        not r->c in b.p1 + b.p2
+        some r.prev_row implies r.prev_row->c in b.p1 + b.p2
+        won[b.p2 + r->c]
+    }
 }
 
 --------------------Win/Turn Tests---------------------
@@ -131,30 +154,34 @@ inst emptyGame {
   p2 = none
 }
 
-//Empty game
+//Empty game checks
 expect {
     wfEmpty1: {wellformed} for exactly emptyGame is sat
     wfEmpty2: {not wellformed} for exactly emptyGame is unsat    
 }
 
+//Horizontal win
 inst horizontal {
     boiler
     p1 = Board -> (Row3 -> Column0 + Row2 -> Column0 + Row2 -> Column1)
     p2 = Board -> (Row3 -> Column1 + Row3 -> Column2 + Row3 -> Column3)
 }
 
+//Vertical win
 inst vertical {
     boiler
     p1 = Board -> (Row3 -> Column0 + Row2 -> Column0 + Row1 -> Column0)
     p2 = Board -> (Row3 -> Column1 + Row3 -> Column2)
 }
 
+//Diagonal win
 inst diagonal {
     boiler
     p1 = Board -> (Row3 -> Column1 + Row3 -> Column2 + Row2 -> Column2)
     p2 = Board -> (Row3 -> Column0 + Row2 -> Column1 + Row1 -> Column2)
 }
 
+//Uneven turns
 inst moreX {
     boiler
     p1 = Board -> (Row3 -> Column0 + Row2 -> Column0 + Row2 -> Column1)
@@ -163,7 +190,9 @@ inst moreX {
 
 //Win tests
 expect {
+    //well formed
     wellformed: {wellformed} for exactly horizontal is sat
+    //all tests either show positive win, or unsat if not
     ht: {won[Board.p2]} for exactly horizontal is sat
     hf: {vertical_w[Board.p1]} for exactly horizontal is unsat
     vt: {won[Board.p1]} for exactly vertical is sat
@@ -173,16 +202,18 @@ expect {
 
 //Turn tests
 expect {
+    //turns are correct for given conditions
     valid: {valid[Board]} for exactly horizontal is sat
     p1: {p1_turn[Board]} for exactly horizontal is sat
     p2: {p2_turn[Board]} for exactly horizontal is unsat
+    //turns are incorrect, extra turn for p1
     moreX: {p1_turn[Board] or p2_turn[Board]} for exactly moreX is unsat
     noWin: {won[Board.p1] or won[Board.p2]} for exactly moreX is unsat
 }
 
 ----------------Trace----------------
 
-//Define initial state of the game
+//Define initial state of the game, no pieces
 state[Board] initialBoard {
     no p1
     no p2
@@ -198,10 +229,21 @@ state[Board] centerStart {
 
 //Initial starts in left corner
 state[Board] cornerStart {
-    one a: Row | no a.prev_row and a in p1.Column //First row has no previous
-    one b: Column | no b.prev_col and b in Row.p1 //Last row has no next
+    one a: Row | no a.prev_row and a in p1.Column 
+    one b: Column | no b.prev_col and b in Row.p1 
     #p1 = 1
     no p2
+}
+
+//Setup so p1 can win next turn, two p1 and two p2 horizontally layered on
+//top of each other
+state[Board] proofStart {
+      some a: (Row - Row.^next_row) | some b: (Column - Column.^next_col) | {
+          (a -> b + a -> b.next_col) in p1
+          (a.next_row -> b + a.next_row -> b.next_col) in p2
+          #p1 = 2
+          #p2 = 2
+      }
 }
 
 //Define parameters for transitioning (making a move)
@@ -242,28 +284,23 @@ transition[Board] moveZeroGrav[r: Row, c: Column] {
     not finished[this]
 }
 
+//Same transition as "move" but players will move to win if they can
 transition[Board] moveIntel[r: Row, c: Column] {
     not r->c in (p1 + p2)
 
-    some r.prev_row implies (r.prev_row)->c in (p1 + p2) //GRAVITY
+    some r.prev_row implies (r.prev_row)->c in (p1 + p2)
     
     p1_turn[this] implies {
-        some a: Row | some b: Column | {
-            not a->b in (p1 + p2)
-            some a.prev_row implies (a.prev_row)->b in (p1 + p2)
-            won[p1 + a->b]
-        } implies r = a and c = b
         p1' = p1 + r->c
         p2' = p2
+        //If p1 can make a move to win, they must do so
+        p1_canwin[this] implies won[p1']
     }
     p2_turn[this] implies {
-        some a: Row | some b: Column | {
-            not a->b in (p1 + p2)
-            some a.prev_row implies (a.prev_row)->b in (p1 + p2)
-            won[p2 + a->b]
-        } implies r = a and c = b
         p2' = p2 + r->c
         p1' = p1
+        //If p2 can make a move to win, they must do so
+        p2_canwin[this] implies won[p2']
     }
 
     not finished[this]
@@ -274,7 +311,7 @@ transition[Board] game {
     some r: Row | some c: Column | move[this, this', r, c]
 }
 
-//moves can be placed anywhere, not "fall"
+//moves can be placed anywhere, won't "fall"
 transition[Board] gameZeroGrav {
     some r: Row | some c: Column | moveZeroGrav[this, this', r, c]
 }
@@ -287,48 +324,20 @@ transition[Board] gameIntel {
 trace<|Board, initialBoard, game, _|> Pure {} //start anywhere, random moves
 trace<|Board, centerStart, game, _|> CenterStartPure {} //start center, random moves
 trace<|Board, cornerStart, game, _|> CornerStartPure {} //start left corner, random moves
+trace<|Board, proofStart, gameIntel, _|> ProofStartIntel  {} //start in proof scenario, intelligent moves
+
+//We only used the above traces for our testing, but you can make traces for any combination
+//of game type, dimension, starting point, and move parameters.
 
 -------------------Trace Tests-----------------------
 
-//run<|Pure|> {wellformed some b: Board | finished[b]} for exactly 3 Row, exactly 3 Column, exactly 6 Board
-
-run<|CornerStartPure|> {wellformed some b: Board | won[b.p1]} for exactly 3 Row, exactly 3 Column, exactly 6 Board
-
 //run {wellformed} for exactly 4 Row, exactly 4 Column, exactly 1 Board
+//run<|Pure|> {wellformed some b: Board | finished[b]} for exactly 3 Row, exactly 3 Column, exactly 6 Board
+//run<|centerStartPure|> {wellformed some b: Board | finished[b]} for exactly 3 Row, exactly 3 Column, exactly 6 Board
+//run<|CornerStartPure|> {wellformed some b: Board | won[b.p1]} for exactly 3 Row, exactly 3 Column, exactly 6 Board
 
-----------------Invariants----------------
-
-pred invariant1 {
-  -- (1) At the start of the game, p1 and p2 indices should be empty
-  initialBoard implies {
-    no p1
-    no p2
-  }
-} 
-
-test expect {
-    --INVAR1: <|T|> {not invariant1} for ... is unsat
+//A test demonstrating that the intelligent move will always  finish the board, and never allow p2 to win
+expect {
+    alwayswin: <|ProofStartIntel|> {wellformed some b: Board | won[b.p2]} for exactly 3 Row, exactly 3 Column, 4 Board is unsat
+    alwayswinopp: <|ProofStartIntel|> {wellformed some b: Board | won[b.p1]} for exactly 3 Row, exactly 3 Column, 4 Board is sat
 }
-
-pred invariant2 {
-  -- (2) Assuming there is gravity, you can't place into a spot without a piece below it
-  all s: LinkedList | 
-    no s.ghost implies {
-      no s.start
-      no s.end
-    }
-} 
-
-test expect {
-    --INVAR2: <|T|> {not invariant2} for ... is unsat
-}
-
-pred invariant3 {
-  -- (3) Two players cannot have put pieces into the same index
-  no Board.p1 & Board.p2
-} 
-
-test expect {
-    --INVAR3: <|T|> {not invariant3} for ... is unsat
-}
-    
